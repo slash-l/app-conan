f8f307abc41ad32f9fd3c03b4255b7e752e9debb
slash-l <slash_mumu@163.com>
2024-04-23 23:06:44 +0800

    conan code update

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # app-conan
-## 1） conan_timer_install
+## 工程说明
+### 1） conan_timer_install
 timer.cpp 定时器，演示 conan install 从 Artifacory 获取三方依赖。
-获取二方 conan_hello_src_upload 的依赖
+获取 Artifactory Conan Local 仓库中的二方包
 
-## 2） conan_hello_src_upload
-hello app 上传至 Artifactory
+
+### 2） conan_hello_src_upload
+通过 Conan 编译 .a 并打包上传至 Artifactory Conan Local 二方仓库
+
+### 3） hello_a_upload
+有时候无法获取源码，只有编译好的 .a 文件，通过 Conan 直接打包并发布到 Artifactory Conan Local 二方仓库
+
+## 演示实操说明
+### 1） 不编译直接打包上传二方库
+
+
+
+
+### 2） 编译并打包上传二方库
+
+
+
+
+
+### 2） 依赖下载三方包和二方包
 
 
--- conan_hello_src_upload/conanfile.py
+++ conan_hello_src_upload/conanfile.py
@@ -1,10 +1,15 @@
 from conans import ConanFile, CMake
 
+# Recipe 会包含以下一些内容
+# source
+# build
+# package
+# package info
 
 class HelloConan(ConanFile):
     name = "hello"
-    # version = "0.4"
-    version = "0.1.BUILD_NUMBER"
+    version = "0.4"
+    # version = "0.1.BUILD_NUMBER"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
@@ -16,8 +21,8 @@ class HelloConan(ConanFile):
     generators = "cmake"
     exports_sources = "src/*"
 
-    # def requirements(self):
-    #     self.requires("poco/1.10.0")    # -> depend on boost 1.74.0
+    def requirements(self):
+        self.requires("poco/1.10.0")    # -> depend on boost 1.74.0
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -29,11 +34,13 @@ class HelloConan(ConanFile):
         cmake.build()
 
         # Explicit way:
+        # 通过 self.run 可以在 conan repice 的任何地方使用、通过本地shell调用任何外部工具命令
         # self.run('cmake %s/hello %s'
         #          % (self.source_folder, cmake.command_line))
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
+        # 将选定的文件从 build 目录复制到 package 目录
         self.copy("*.h", dst="include", src="src")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
@@ -41,5 +48,6 @@ class HelloConan(ConanFile):
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
+    # package_info 定义将传递给包使用者的变量
     def package_info(self):
         self.cpp_info.libs = ["hello"]
